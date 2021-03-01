acd5994aa4673069189bc64e0c1da2f998ccd79c
ashlinrichardson <richardson.ashlin@gmail.com>
2021-02-28 21:43:20 -0800

    succeed if repo url reqd

--- setup.R
+++ setup.R
@@ -1,7 +1,24 @@
+# edited this to add resilience. Should be able to clean this up better
 if("V8" %in% rownames(installed.packages()) == FALSE){
+  result = tryCatch({
     install.packages("V8")
+  }, warning = function(warning_condition) {
+    # warning-handler-code
+  }, error = function(error_condition) {
+    install.packages("V8", repo="http://cran.rstudio.com/")
+  }, finally={
+    # cleanup-code
+  })
 }
 
 if("Rcpp" %in% rownames(installed.packages()) == FALSE){
+  result = tryCatch({
     install.packages("Rcpp")
+  }, warning = function(warning_condition) {
+    #warning-handler-code
+  }, error = function(error_condition) {
+   install.packages("Rcpp", repo="http://cran.rstudio.com/")
+  }, finally={
+    #cleanup-code
+  })
 }
