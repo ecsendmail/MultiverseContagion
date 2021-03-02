ba6c164a8f495a0a7771dd99853c8fe295810878
Ashlin Richardson <richardson.ashlin@gmail.com>
2021-03-01 22:58:03 -0800

    update

--- setup.R
+++ setup.R
@@ -1,24 +1,22 @@
-# edited this to add resilience. Should be able to clean this up better
-if("V8" %in% rownames(installed.packages()) == FALSE){
-  result = tryCatch({
-    install.packages("V8")
-  }, warning = function(warning_condition) {
-    # warning-handler-code
-  }, error = function(error_condition) {
-    install.packages("V8", repo="http://cran.rstudio.com/")
-  }, finally={
-    # cleanup-code
-  })
+# install package defaulting to rstudio server if need be 
+install_package<-function(x){
+  if(x %in% rownames(installed.packages()) == FALSE){
+    result = tryCatch({
+      install.packages(x)
+    }, warning = function(warning_condition) {
+      # warning-handler-code
+    }, error = function(error_condition) {
+      install.packages(x, repo="http://cran.rstudio.com/")
+    }, finally={
+      # cleanup-code
+    })
+  }
+  else{
+    cat("Already installed: ", x, "\n")
+  } 
 }
 
-if("Rcpp" %in% rownames(installed.packages()) == FALSE){
-  result = tryCatch({
-    install.packages("Rcpp")
-  }, warning = function(warning_condition) {
-    #warning-handler-code
-  }, error = function(error_condition) {
-   install.packages("Rcpp", repo="http://cran.rstudio.com/")
-  }, finally={
-    #cleanup-code
-  })
-}
+# set up dependencies
+install_package("V8")
+install_package("Rcpp")
+install_package("parallel")
