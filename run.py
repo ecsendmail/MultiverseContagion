4e4568e5ddd3b6c38a8ac82e7bbff306df18e1e8
Ashlin Richardson <richardson.ashlin@gmail.com>
2021-03-01 23:09:04 -0800

    update

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ CPU_COUNT = os.cpu_count()
 
 f = open('run.sh', 'wb')
 for i in range(N_SIMULATIONS):
-    f.write(('Rscript run.R ' + str(i + 1) + ' > log_' + str(i) + '.txt 2>&1 &\n').encode())
+    f.write(('Rscript run.R ' + str(i + 1) + ' > log_' + str(i + 1) + '.txt 2>&1 &\n').encode())
     if (i + 1) % CPU_COUNT == 0:
         f.write('wait\n'.encode())
 f.close()
