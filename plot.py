a09fdead9dc77d24bd6b15c9cc632249401825a8
Ashlin Richardson <richardson.ashlin@gmail.com>
2021-03-01 23:41:11 -0800

    update

--- plot.py
+++ plot.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 d = {}
 max_N = 0
 files = [x.strip() for x in os.popen("ls -1 count*.csv").readlines()]
+print(len(files), "number of files")
 for f in files:
     lines = [x.strip() for x in open(f).readlines()]
     exp = '"","green","yellow","blue","red","orange"'
@@ -24,8 +25,43 @@ for f in files:
 
     N = len(data) - 1
     if N > max_N:
-        max_N = N
-
+        max_N = N 
     d[f] = data
 
+value = [np.zeros(max_N) for i in range(5)]
+print(value)
+
 print('max_N', max_N)
+
+ci = 1
+for f in files:
+    print(ci, f)
+    ci += 1
+    data = d[f]
+    for i in range(len(data) - 1):
+        w = data[i + 1]
+        j = w[0] - 1
+        w = w[1:]
+        for k in range(len(w)):
+            value[k][j] += w[k]
+
+N = float(len(files))
+lab = ["green","yellow","blue","red","orange"]
+for k in range(len(value)):
+    plt.plot(value[k] / N, color=lab[k], label=lab[k])
+plt.legend()
+plt.title("average count per state over " + str(int(N)) + "simulations")
+plt.show()
+
+'''
+"","green","yellow","blue","red","orange"
+"1",100,0,0,0,0
+"2",100,0,0,0,0
+"3",96,0,4,0,0
+"4",95,0,5,0,0
+"5",95,0,5,0,0
+"6",95,0,5,0,0
+"7",95,0,5,0,0
+"8",95,0,5,0,0
+"9",95,0,5,0,0
+'''
