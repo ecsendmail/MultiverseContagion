f71be5c863416d25c7b26c4d7e92d13e6f92d49a
Ashlin Richardson <richardson.ashlin@gmail.com>
2021-03-01 23:05:48 -0800

    update

---
+++ run.py
@@ -0,0 +1,17 @@
+# run R simulation wrapper, in parallel
+import os
+import sys
+
+N_SIMULATIONS = 10
+CPU_COUNT = os.cpu_count()
+
+f = open('run.sh', 'wb')
+for i in range(N_SIMULATIONS):
+    f.write(('Rscript run.R ' + str(i + 1) + ' & > log_' + str(i) + '.txt\n').encode())
+    if (i + 1) % CPU_COUNT == 0:
+        f.write('wait\n'.encode())
+f.close()
+
+a = os.system('chmod 755 run.sh')
+a = os.system('./run.sh')
+
